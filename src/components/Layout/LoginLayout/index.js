289fac51eb284e329b1d002668a4ce0c4ae99fdf
HuyDeveloper <vinhhuy16072002@gmail.com>
2023-07-17 22:38:17 +0700

    Login form

--- src/components/Layout/LoginLayout/index.js
+++ src/components/Layout/LoginLayout/index.js
@@ -1,13 +1,52 @@
-import classNames from 'classnames';
-import styles from './LoginLayout.module.scss';
+import { useEffect, useState } from 'react';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faCar, faHouseChimney, faMotorcycle } from '@fortawesome/free-solid-svg-icons';
+import styles from '../components/Header/Header.module.scss';
+import Menu from '~/components/Popper/Menu';
+
 const cx = classNames.bind(styles);
-function LoginLayout({ children }) {
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faHouseChimney} />,
+        title: 'Home',
+        to: '/',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCar} />,
+        title: 'Booking cars',
+        to: '/booking-cars',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faMotorcycle} />,
+        title: 'Booking bikes',
+        to: '/booking-bikes',
+    },
+];
+
+function Header({ children }) {
+    const handleMenuChange = (menuItem) => {
+        console.log(menuItem);
+    };
     return (
         <div>
-            <h1>Header Login</h1>
-            <div className={cx('content')}>{children}</div>
+            <header className={cx('wrapper')}>
+                <div className={cx('inner')}>
+                    <h2 className={cx('logo')}>Uber</h2>
+
+                    <div className={cx('actions')}>
+                        <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faBars} />
+                            </button>
+                        </Menu>
+                    </div>
+                </div>
+            </header>
+            <div className="content">{children}</div>
         </div>
     );
 }
 
-export default LoginLayout;
+export default Header;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,48 @@
+import classNames from 'classnames/bind';
+import styles from './login.module.scss';
+import Button from '~/components/Button';
+import { useState } from 'react';
+const cx = classNames.bind(styles);
+
 function Login() {
-    return <h2>Login page</h2>;
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    console.log(phone, password);
+    const handleSubmit = () => {
+        console.log('Input value:', phone, password);
+    };
+    return (
+        <div className={cx('wrapper')}>
+            <div>
+                <form className={cx('inner')} onSubmit={handleSubmit}>
+                    <div className={cx('login-form')}>
+                        <div className={cx('logo')}>Sign in</div>
+
+                        <input
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                            className={cx('input')}
+                            type="text"
+                            name="phone"
+                            placeholder="Phone number"
+                        />
+                        <input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className={cx('input')}
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                        />
+                        <Button type="submit" primary rounded large>
+                            Log in
+                        </Button>
+                    </div>
+                    <div className={cx('note')}>*Note: Must be sign in as administrator</div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
